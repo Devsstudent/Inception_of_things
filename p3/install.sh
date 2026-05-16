d275d4e006a004fe55dc85aaf2dc7ce1d27112ea
Devsstudent <devsstudent777@gmail.com>
2026-05-16 18:38:20 +0200

    installing argo setup

--- p3/install.sh
+++ p3/install.sh
@@ -10,3 +10,14 @@ kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
 # this give the password and the username is admin
+
+docker pull wil42/playground:v1
+docker pull wil42/playground:v2
+
+kubectl create -f ./pod.yaml
+
+
+kubectl config set-context --current --namespace=argocd
+
+#argocd login  http://localhost:8080
+
