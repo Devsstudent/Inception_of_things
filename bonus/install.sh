d0f5158923acf4461102f0876d8ecbd41a814ac3
Devsstudent <devsstudent777@gmail.com>
2026-05-17 19:16:22 +0200

    bonus accessing ui gitlab

---
+++ bonus/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+k3d cluster create --config k3d-config.yaml
+kubectl create namespace gitlab
+kubectl create namespace dev
+kubectl create namespace argocd
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl create -f ./deployment_service.yaml
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
+# this give the password and the username is admin
+
+docker pull wil42/playground:v1
+docker pull wil42/playground:v2
+
+kubectl create -f ./pod.yaml
+
+
+kubectl config set-context --current --namespace=argocd
+
+#argocd login  http://localhost:8080
+
